f17a43e27b2a6e15fc4d42583fe282f285606591
ganomin <szymon.wirkus12@gmail.com>
2024-12-08 18:13:37 +0000

    lime: Include and build android_hardware_xiaomi
    Signed-off-by: ganomin <szymon.wirkus12@gmail.com>

--- device.mk
+++ device.mk
@@ -80,7 +80,8 @@ PRODUCT_COPY_FILES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/xiaomi
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/lime/lime-vendor.mk)
