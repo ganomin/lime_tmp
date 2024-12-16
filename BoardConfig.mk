3994e5ab522b2c8ae8195dfa4fd01e611bf36b97
Michael Bestas <mikeioannina@gmail.com>
2024-12-16 14:16:28 +0000

    lime: Include Lineage FCM
    Signed-off-by: ganomin <szymon.wirkus12@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -133,7 +133,8 @@ DEVICE_MATRIX_FILE += \
     hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
-    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
+    vendor/lineage/config/device_framework_matrix.xml
 
 # Inherit the proprietary files
 include vendor/xiaomi/lime/BoardConfigVendor.mk
