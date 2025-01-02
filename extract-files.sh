9f3f92756a550388e7bd6fb57a39e9c61b2b5709
ganomin <szymon.wirkus12@gmail.com>
2025-01-02 13:42:58 +0000

    lime: Adapt proprietary-files to lime vendor
    Signed-off-by: ganomin <szymon.wirkus12@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -7,10 +7,6 @@
 
 function blob_fixup() {
     case "${1}" in
-        vendor/etc/init/init.batterysecret.rc)
-            [ "$2" = "" ] && return 0
-            sed -i "/seclabel u:r:batterysecret:s0/d" "${2}"
-            ;;
         vendor/etc/libnfc-nci.conf)
             [ "$2" = "" ] && return 0
             grep -q "LEGACY_MIFARE_READER=1" "${2}" || cat << EOF >> "${2}"
@@ -32,10 +28,6 @@ EOF
         vendor/lib64/hw/camera.qcom.so)
             [ "$2" = "" ] && return 0
             sed -i "s/\x73\x74\x5F\x6C\x69\x63\x65\x6E\x73\x65\x2E\x6C\x69\x63/\x63\x61\x6D\x65\x72\x61\x5F\x63\x6E\x66\x2E\x74\x78\x74/g" "${2}"
-            ;;
-        vendor/lib64/camera/components/com.mi.node.watermark.so)
-            [ "$2" = "" ] && return 0
-            grep -q "libpiex_shim.so" "${2}" || "${PATCHELF}" --add-needed "libpiex_shim.so" "${2}"
             ;;
         *)
             return 1
